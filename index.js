9e46f71de915c9349196d3706596911b5db40bb3
dani-mr <dmartinezra@icloud.com>
2022-05-20 21:49:56 +0200

    feature added: cartel change info/color on click

--- index.js
+++ index.js
@@ -15,27 +15,59 @@ const llamadaApi = async () => {
   buscador$$.addEventListener("input", () => buscar(datosApiJSON));
   //al añadir () => hacemos que solo se ejecute al hacer input
   pintar(datosApiJSON);
+};
 
+const detalle = (pelicula, cartel$$) => {
+  if (cartel$$.className === "cartel") {
+
+    cartel$$.className = "cartelPinchado";
+
+    cartel$$.innerHTML = `
+
+    <h2 class="titulo">${pelicula.title}</h2>
+    <img class="imagen" src="${pelicula.image}" alt="${pelicula.title}" />
+    <div class="informacion">
+      <p>${pelicula.director}</p>
+      <p>${pelicula.running_time}</p>
+    </div>
+
+    `;
+  } else {
+
+    cartel$$.className = "cartel";
+
+    cartel$$.innerHTML = `
+
+        <h2 class="titulo">${pelicula.title}</h2>
+        <img class="imagen" src="${pelicula.image}" alt="${pelicula.title}" />
+        <div class="informacion">
+          <p>${pelicula.release_date}</p>
+          <p>${pelicula.rt_score}</p>
+        </div>
+    `;
+
+  }
 };
 
 const buscar = (peliculas) => {
-
   const peliculasFiltradas = [];
 
   //Iteramos sobre peliculas para que cuando haga la búsqueda, inserte las películas en el array correspondientes a la película
   for (const pelicula of peliculas) {
     //Si la búsqueda incluye el valor de pelicula.title, lo pushea en el array vacio
-    if (pelicula.title.toLowerCase().includes(buscador$$.value.toLowerCase().trim())) {
+    if (
+      pelicula.title
+        .toLowerCase()
+        .includes(buscador$$.value.toLowerCase().trim())
+    ) {
       peliculasFiltradas.push(pelicula);
     }
   }
 
   pintar(peliculasFiltradas);
-
 };
 
 const pintar = (peliculas) => {
-
   //Con este innerHTML, borramos todo el HTML en la ejecución
   cartelera$$.innerHTML = ``;
 
@@ -58,6 +90,7 @@ const pintar = (peliculas) => {
     //Añadimos la clase cartel a el div creado en cartel$$
 
     cartel$$.className = "cartel";
+    cartel$$.addEventListener("click", () => detalle(pelicula, cartel$$));
 
     //OPCION 2 - usando document.createElement(), añadiendo clases y atributos a mano y usando appenChild() para pintarlo en el HTML
 
