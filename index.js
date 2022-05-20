afc8c353cbf5c763a261add45aa63f87a76effcc
dani-mr <dmartinezra@icloud.com>
2022-05-20 21:26:43 +0200

    styles modified & shearch bar added

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const cartelera$$ = document.querySelector(".cartelera");
-
-console.log(cartelera$$);
-
-
-let peliculas;
+const buscador$$ = document.querySelector(".input");
 
 //necesito una función asincrona para hacer la llamada a la api (fetch)
 const llamadaApi = async () => {
-
   const datosApi = await fetch("https://ghibliapi.herokuapp.com/films");
   const datosApiJSON = await datosApi.json();
 
@@ -16,39 +11,79 @@ const llamadaApi = async () => {
   //     console.log(misDatos);
   // });
 
+  //Para fixear que no ejecute la función sin hacer el input, usamos esta sintáxis
+  buscador$$.addEventListener("input", () => buscar(datosApiJSON));
+  //al añadir () => hacemos que solo se ejecute al hacer input
   pintar(datosApiJSON);
+
+};
+
+const buscar = (peliculas) => {
+
+  const peliculasFiltradas = [];
+
+  //Iteramos sobre peliculas para que cuando haga la búsqueda, inserte las películas en el array correspondientes a la película
+  for (const pelicula of peliculas) {
+    //Si la búsqueda incluye el valor de pelicula.title, lo pushea en el array vacio
+    if (pelicula.title.toLowerCase().includes(buscador$$.value.toLowerCase().trim())) {
+      peliculasFiltradas.push(pelicula);
+    }
+  }
+
+  pintar(peliculasFiltradas);
 
 };
 
 const pintar = (peliculas) => {
 
+  //Con este innerHTML, borramos todo el HTML en la ejecución
+  cartelera$$.innerHTML = ``;
+
   for (const pelicula of peliculas) {
-
     const cartel$$ = document.createElement("div");
-    const titulo$$ = document.createElement("h2");
-    const imagen$$ = document.createElement("img");
-    const info$$ = document.createElement("div");
-    const year$$ = document.createElement("p");
-    const rating$$ = document.createElement("p");
 
-    cartel$$.classList.add("cartel");
-    titulo$$.classList.add("titulo");
-    titulo$$.textContent = pelicula.title;
-    imagen$$.classList.add("imagen");
-    imagen$$.setAttribute("src", pelicula.image);
-    imagen$$.setAttribute("alt", pelicula.title);
-    info$$.className = "informacion";
-    year$$.innerHTML = `${pelicula.release_date}`;
-    rating$$.innerText = pelicula.rt_score;
+    //OPCIÓN 1 (más optimizada) - usamos un solo innerHTML para que itere sobre el y pinte cada uno de los carteles. En el, agregamos todas las clases y atributos
+    //a tener en cuenta...
 
+    cartel$$.innerHTML = `
 
-    info$$.appendChild(year$$);
-    info$$.appendChild(rating$$);
-    cartel$$.appendChild(titulo$$);
-    cartel$$.appendChild(imagen$$);
-    cartel$$.appendChild(info$$);
+        <h2 class="titulo">${pelicula.title}</h2>
+        <img class="imagen" src="${pelicula.image}" alt="${pelicula.title}" />
+        <div class="informacion">
+          <p>${pelicula.release_date}</p>
+          <p>${pelicula.rt_score}</p>
+        </div>
+    `;
+
+    //Añadimos la clase cartel a el div creado en cartel$$
+
+    cartel$$.className = "cartel";
+
+    //OPCION 2 - usando document.createElement(), añadiendo clases y atributos a mano y usando appenChild() para pintarlo en el HTML
+
+    // const titulo$$ = document.createElement("h2");
+    // const imagen$$ = document.createElement("img");
+    // const info$$ = document.createElement("div");
+    // const year$$ = document.createElement("p");
+    // const rating$$ = document.createElement("p");
+
+    // cartel$$.classList.add("cartel");
+    // titulo$$.classList.add("titulo");
+    // titulo$$.textContent = pelicula.title;
+    // imagen$$.classList.add("imagen");
+    // imagen$$.setAttribute("src", pelicula.image);
+    // imagen$$.setAttribute("alt", pelicula.title);
+    // info$$.className = "informacion";
+    // year$$.innerHTML = `${pelicula.release_date}`;
+    // rating$$.innerText = pelicula.rt_score;
+
+    // info$$.appendChild(year$$);
+    // info$$.appendChild(rating$$);
+    // cartel$$.appendChild(titulo$$);
+    // cartel$$.appendChild(imagen$$);
+    // cartel$$.appendChild(info$$);
+
     cartelera$$.appendChild(cartel$$);
-
   }
 };
 
